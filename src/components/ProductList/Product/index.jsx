6af05ff0f5f9ebb752701f6080ab4ece63f63882
buiducviettt <ducviet8a4@gmail.com>
2024-10-31 11:41:03 +0700

    again

--- src/components/ProductList/Product/index.jsx
+++ src/components/ProductList/Product/index.jsx
@@ -1,23 +1,7 @@
 import Button from '../../Button';
 import styles from '../../ProductList/product.module.scss';
 
-const ProductItem = ({ title, desc, image }) => {
-  return (
-    <div
-      className={styles.itemWrapper}
-      data-aos="flip-left"
-      data-aos-once="true"
-      data-aos-offset="300"
-    >
-      <div className={styles.inner}>
-        <h1>{title}</h1>
-        <p>{desc}</p>
-        <Button title="Xem thêm" className={styles.showMoreBtn} />
-        <div className="mt-3">
-          <img src={image} alt="" />
-        </div>
-      </div>
-    </div>
-  );
+const ProductItem = () => {
+  return <h1>Hello</h1>;
 };
 export default ProductItem;
